88f4a84b0280022e3de38edde8bd97d278470991
pratikderepatil <pratikdere333@gmail.com>
2023-03-10 21:51:57 +0530

    File upload working at backend

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, "uploads");
+		},
+		filename: function (req, file, cb) {
+			cb(null, file.originalname);
+		},
+	}),
+}).single("fileupload");
+
+app.post("/upload", upload, (req, res) => {
+	res.send("File uploaded successfully!");
+});
+
+app.get("/files", upload, (req, res) => {
+	const testFolder = "./uploads/";
+	const fs = require("fs");
+	fs.readdir(testFolder, (err, files) => {
+		res.send(files);
+	});
+});
+
+app.listen(8000, () => {
+	console.log("listening on https://localhost:8000");
+});
